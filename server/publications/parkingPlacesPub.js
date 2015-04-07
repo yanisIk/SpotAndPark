520f24f2cacc947296c5ea191ea0febe8124923a
Yanis <yanis.ikene.1@etsmtl.net>
2015-04-06 22:36:31 -0400

    added data example in data parser

--- server/publications/parkingPlacesPub.js
+++ server/publications/parkingPlacesPub.js
@@ -1,41 +1,4 @@
 
-//Publish from HTTP API
-Meteor.publish('parkingPlacesFromAPI', function(query) {
-    var self = this;
-    try {
-    //TORONTO PARKING LIST API
-        // http://www1.toronto.ca/City%20Of%20Toronto/Information%20&%20Technology/Open%20Data/Data%20Sets/Assets/Files/greenPParking2015.json
-        var response = HTTP.get('STATIONNEMENT MONTREAL API URL',{
-            timeout: 5000,
-                params:{
-                "format": "json",
-                    "access_token": Meteor.settings.stationnementMtlApiKey
-            }
-        });
-
-        if(response.statusCode === 200) {
-
-            //Construire le document
-            _.each(response.data.items, function (item) {
-                var doc = {
-                    /*
-                     id: item.,
-                     coordinates: item.,
-                     payed: item.,
-                     */
-                };
-
-                self.added('places', Random.id(), doc);
-            });
-
-            self.ready();
-        }
-    } catch(error) {
-        console.log(error);
-    }
-});
-
-
 Meteor.publish("allParkingPlaces", function(city){
    check(city, String);
     return ParkingPlaces.find({city: city});
--- server/seeds/parkingDataParser.js
+++ server/seeds/parkingDataParser.js
@@ -14,3 +14,33 @@ Meteor.methods({
        });
    }
 });
+
+
+/**
+ //DANS FICHIER signalisation-geoloc-poteau-CDNNDG.json
+ {
+    "type":"Feature",
+    "geometry":{"type":"Point",
+                "coordinates":[295426.719,5036939.5]},
+    "properties":{"POTEAU_ID_POT":93007,
+                  "DATE_CONCEPTION_POT":20120731000000,
+                  "DESCRIPTION_REP":"Réel",
+                  "DESCRIPTION_RTP":"3- Fût",
+                  "X":295426.719,
+                  "Y":5036939.5,
+                  "ID_ARRONDISSEMENT":"34",
+                  "TRC_ID":1310267}
+}
+
+
+ //DANS FICHIER signalisation-description-panneau.json
+ {
+    "json_featuretype":"signalisation-description-panneau"
+    ,"POTEAU_ID_POT":54
+    ,"PANNEAU_ID_PAN":57
+    ,"DESCRIPTION_RPA":"\\A 16h-18h30 LUN. AU VEN."
+    ,"CODE_RPA":"AV-PF" //IMAGE
+    ,"FLECHE_PAN":0
+    ,"POSITION_POP":7
+}
+ **/
